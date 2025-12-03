03fa1971cf91f1a6ba7f258774aab3bcdbab6cb4
PDK <coson35@daum.net>
2025-12-03 11:17:38 +0900

    deploy static export

---
+++ _next/static/R3Mh61l_QPd3xsJUzS29I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/552-f250d6c2d7a98958.js","static/chunks/pages/index-a92c80c62a9d6c0e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
